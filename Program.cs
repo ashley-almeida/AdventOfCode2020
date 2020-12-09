36cb6f8bb460e20e28bba9f10e12565836e2563e
ashualmeida <almeida.ashley1@gmail.com>
2020-12-09 16:33:10 +0100

    Updating for the first time

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            new day8().dostuffPart2();
+        }
+    }
+}
---
+++ day1.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Linq;
+public class day1
+{
+    public static void dostuff()
+    {
+        
+    }
+}
