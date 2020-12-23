94d73b3b352432e2557f73335d968db93988fd90
almeida.ashley1@gmail.com <almeida.ashley1@gmail.com>
2020-12-23 16:30:09 +0100

    day15

--- Day15.cs
+++ Day15.cs
@@ -11,13 +11,44 @@ public class Day15
 #pragma warning disable 219
     public void DoStuffPartOne()
     {
-        
+        string sampleString = "0,3,6";
+        string inputString = "9,6,0,10,18,2,1";
+        var counterDict = new Dictionary<int, Dictionary<int,int>>();
+        var inputs = sampleString.Split(',').Select(int.Parse).ToList();
+
+        var turnNumber = 0;
+
+        for (; turnNumber < inputs.Count; turnNumber++)
+        {
+            counterDict.Add(inputs[turnNumber], -1);
+        }
+
+        var lastNumber = inputs[-1];
+
+        for (int i = inputs.Count; i < 2020; i++)
+        {
+            if (counterDict.TryGetValue(lastNumber, out int spoken))
+            {
+                if (spoken == -1)
+                {     
+                    lastNumber = 0;               
+                    if (counterDict.TryGetValue(0, out int zeroSpoken))
+                        counterDict[0] = i+1;
+                    else
+                        counterDict.Add(0, -1);
+                }
+                else
+                {
+
+                }
+            }
+        }
     }
 
-    
+
 
     public void DoStuffPartTwo()
     {
-        
+
     }
 }
